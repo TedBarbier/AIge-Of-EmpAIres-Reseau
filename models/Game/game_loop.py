c0e9339cc5911a4969030feb9f0ab3ce983f9748
jules-chd <jules.cochard63@gmail.com>
2025-03-19 16:12:39 +0100

    fonction pour recevoir des messages sur le port 1234

--- models/Game/game_loop.py
+++ models/Game/game_loop.py
@@ -38,6 +38,8 @@ class GameLoop:
         self.num_players = 1
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
         self.udp_socket.bind(("127.0.0.1", 12345))
+        self.udp_socket_to_receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_socket_to_receive.bind(("127.0.0.1", 1234))
         self.polygon = None
     
     def add_new_player(self):
@@ -48,7 +50,7 @@ class GameLoop:
 
     def handle_new_players(self):
         buffersize = 1024
-        readable, _ , _ = select.select([self.udp_socket], [], [], 0.1) 
+        readable, _ , _ = select.select([self.udp_socket_to_receive], [], [], 0.1) 
         for s in readable:
             data, addr = s.recvfrom(buffersize)
             if data:
@@ -56,7 +58,17 @@ class GameLoop:
                 received_message = data.decode('utf-8')
                 if received_message == "Rejoindre la partie":
                     self.add_new_player()
-
+                    print("Nouveau joueur ajoute")
+    
+    def handle_message(self):
+        buffersize = 1024
+        readable, _ , _ = select.select([self.udp_socket_to_receive], [], [], 0.1) 
+        for s in readable:
+            data, addr = s.recvfrom(buffersize)
+            if data:
+                print("Message reçu : ", data)
+                received_message = data.decode('utf-8')
+                return(received_message)
 
     def handle_start_events(self, event):
         if pygame.key.get_pressed()[pygame.K_F12]:
@@ -326,6 +338,7 @@ class GameLoop:
                 self.update_game_state(dt)
                 if self.state.is_multiplayer and self.num_players < self.state.selected_players:
                     self.handle_new_players()
+                self.handle_message()
             self.render_display(dt, mouse_x, mouse_y)
 
 
