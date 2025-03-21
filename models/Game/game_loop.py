ad5d98937ab7e3362afdf460a645b2a2408d8a1f
jules-chd <jules.cochard63@gmail.com>
2025-03-21 10:35:32 +0100

    map_send

--- models/Game/game_loop.py
+++ models/Game/game_loop.py
@@ -95,7 +95,15 @@ class GameLoop:
                 else:
                     return(received_message)
             
-
+    def create_info_entity(self):
+        representation_list_letter=['v','h', 'a', 's', 'x', 'm', 'c', 'T', 'H', 'C', 'F', 'B', 'S', 'A', 'K', 'W', 'G']
+        map_dict={}
+        for representation in representation_list_letter:
+            map_send=self.state.map.players_dict[1].entities_dict.get(representation, None)
+            if map_send is not None:
+                for key in map_send.keys():
+                    map_dict[key]=map_send[key].to_network_dict()
+        return map_dict
 
     def handle_start_events(self, event):
         if pygame.key.get_pressed()[pygame.K_F12]:
@@ -154,6 +162,8 @@ class GameLoop:
                     # "player_dict" : self.state.map.players_dict
                 }}
 
+                print("create",self.create_info_entity())
+                map_send=self.create_info_entity()
                 self.reseau.send_action_via_udp("Envoi de la map")
                 self.reseau.send_action_via_udp(map_send)
 
@@ -408,6 +418,7 @@ class GameLoop:
 
             if self.state.states == PLAY:
                 self.update_game_state(dt)
+                self.reseau.send_action_via_udp(self.create_info_entity())
                 if self.state.is_multiplayer:
                     self.handle_message()
             self.render_display(dt, mouse_x, mouse_y)
