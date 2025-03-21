c285a8f04e81b24297997b6c0eb0bc9191264539
TedBarbier <ted.barbier@proton.me>
2025-03-21 16:05:23 +0100

    remove elif

--- models/Game/game_loop.py
+++ models/Game/game_loop.py
@@ -85,12 +85,9 @@ class GameLoop:
                     self.num_players = dict["Map"]["nb_player"]
                     self.state.start_game(self.num_players)
                 elif "representation" in received_message:
-                    print("received players")
+                    #print("received players")
                     dict = self.string_to_dict(received_message)
                     self.state.map.create_entity(dict)
-                elif received_message == "\"Rejoindre la partie\"" and self.num_players < self.state.selected_players:
-                        self.add_new_player()
-                        self.reseau.send_action_via_udp("Vous avez rejoint la partie")
                 else:
                     return(received_message)
             
