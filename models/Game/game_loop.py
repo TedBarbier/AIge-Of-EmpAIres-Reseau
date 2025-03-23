02024d9c46950591703f76a94b95c568d42a852e
TedBarbier <ted.barbier@proton.me>
2025-03-23 11:47:53 +0100

    patch resources test modifying

--- models/Game/game_loop.py
+++ models/Game/game_loop.py
@@ -111,14 +111,9 @@ class GameLoop:
                         wood = dict["get_context_to_send"]["resources"]["wood"] - player.get_current_resources()["wood"]
                         food = dict["get_context_to_send"]["resources"]["food"] - player.get_current_resources()["food"]
                         if gold > 0:
-                            player.add_resource({"gold", resources["gold"]})
-                        if gold > 0:
                             player.add_resources(gold)
                         else:
-                            player.remove_resource({"gold", resources["gold"]})
-                        if wood > 0:
-                            player.add_resource({"wood", resources["wood"]})
-                            player.remove_resource(gold)
+                            player.remove_resources(gold)
                         if wood > 0:
                             player.add_resources(wood)
                         else:
