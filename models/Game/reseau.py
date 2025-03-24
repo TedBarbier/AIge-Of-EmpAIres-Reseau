f850bcc2d0bfe809922cfe5fb8a151ffc7ea7388
mathisvel <mathis.velez1111@gmail.com>
2025-03-24 23:40:38 +0100

    test des paquets

--- models/Game/reseau.py
+++ models/Game/reseau.py
@@ -9,6 +9,9 @@ class Send:
         # Création du socket UDP
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.udp_socket.setblocking(0)
+        # Compteurs de paquets
+        self.packets_sent = 0
+        self.packets_received = 0
 
     def send_action_via_udp(self, context_to_send):
             try:
@@ -18,6 +21,8 @@ class Send:
                 action_json = json.dumps(action_data)  # Convertir en JSON
                 #print(f"Envoi de : {action_json} à {self.udp_host}:{self.udp_port} \n")
                 self.udp_socket.sendto(action_json.encode('utf-8'), (self.udp_host, self.udp_port))
+                self.packets_sent += 1
+                print(f"Paquets envoyés: {self.packets_sent}, Paquets reçus: {self.packets_received}")
                 #print(f"Action envoyée via UDP : {action_json} \n")
             except BlockingIOError:
                 # Handle the error if the socket is non-blocking and the send buffer is full
